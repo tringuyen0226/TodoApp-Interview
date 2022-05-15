0b11b4466ff6babac0ddd3a1d0351656581c3b3d
Nguyễn Thanh Trí <thanhtri7637@gmail.com>
2022-05-15 23:37:59 +0700

    Init project

---
+++ TodoApp/TodoApp/Base/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  TodoApp
+//
+//  Created by NguyenThanhTri on 15/05/2022.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ TodoApp/TodoApp/Scences/Home/View/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  TodoApp
+//
+//  Created by NguyenThanhTri on 15/05/2022.
+//
+
+import UIKit
+
+final class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
